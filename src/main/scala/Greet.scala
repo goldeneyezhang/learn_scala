f31cce5877772067eeb178c3feb3129bf49c40b4
yibo <goldeneyezhang@163.com>
2019-06-15 22:19:43 +0800

    第三章

---
+++ src/main/scala/Greet.scala
@@ -0,0 +1,14 @@
+object Greet {
+  def main(args: Array[String]) = {
+    println("Hello world!")
+    //sample 3.1
+    val greetStrings = new Array[String](3)
+    greetStrings(0) = "Hello"
+    greetStrings(1) = ", "
+    greetStrings(2) = "world!\n"
+    for(i <- 0 to 2)
+      print(greetStrings(i))
+
+
+  }
+}
